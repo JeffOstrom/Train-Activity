91c7e870fbd0bf5d78c0701f2b33e58e519984e4
Jeff Ostrom <j.ostrm@gmail.com>
2017-10-21 09:43:14 -0500

    app.js firebase linking testing

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+console.log("test");
+
+//Global Variables List
+var name = "";
+var destination = "";
+var frequency = "";
+var next = "";
+var minutes = "";
+
+
+
+//Database global variables firebase 
+  var config = {
+    apiKey: "AIzaSyAMVx3-K9zSm48921bWhMimOk1zqwsfRcM",
+    authDomain: "train-activity-6c89f.firebaseapp.com",
+    databaseURL: "https://train-activity-6c89f.firebaseio.com",
+    projectId: "train-activity-6c89f",
+    storageBucket: "train-activity-6c89f.appspot.com",
+    messagingSenderId: "119614708109"
+  };
+  firebase.initializeApp(config);
